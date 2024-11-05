07e3d3dcc173db73e3ecd1f02fc12772f0fdb5fe
htkiet <kiet.the@emandai.net>
2024-11-05 18:11:08 +0700

    Improve chunking and formatting documents

--- utils/formatter.py
+++ utils/formatter.py
@@ -1,14 +1,16 @@
+import re
 from typing import List
 from langchain_core.documents import Document
 
 
 def format_docs(docs: List[Document]) -> str:
     """Format the retrieved documents"""
+    docs = sorted(docs, key=lambda doc: doc.metadata['page'] * 1e6 + doc.metadata['start_index'])
     return "\n".join(doc.page_content for doc in docs)
 
 
 def _snippet(doc: Document) -> str:
-    lines = [line for line in doc.page_content.split("\n") if len(line) > 0]
+    lines = [line for line in re.split(r'\. |\! |\? |\n', doc.page_content) if len(line) > 0]
     q = lines[0]
     a = lines[1]
     return f"{q}\n{a} […]"
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -22,24 +22,32 @@ def create_vector_data(embed_model: HuggingFaceEmbeddings,
     text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"],
                                                    chunk_size=embed_tokens,
                                                    chunk_overlap=chunk_overlap,
-                                                   length_function=token_counter)
+                                                   length_function=token_counter,
+                                                   add_start_index=True)
 
     # Read data
     with open("./data/raw/iucn_data.txt") as f:
         docs = f.read().split("\n--\n")
         print("* Number of documents:", len(docs))
 
-    splitted_documents = text_splitter.create_documents(docs)
-    splitted_documents = [Document(doc.page_content, metadata={"len": token_counter(doc.page_content)})\
-                          for doc in splitted_documents]
-    print("* Number of splitted documents:", len(splitted_documents))
+    # Chunk data
+    chunks = text_splitter.create_documents(docs)
+    page = 0
+    for i in range(len(chunks)):
+        _metadata = chunks[i].metadata
+        if _metadata['start_index'] == 0:
+            page += 1
+        _metadata['page'] = page
+        _doc = Document(chunks[i].page_content, metadata=_metadata)
+        chunks[i] = _doc
+    print("* Number of splitted documents:", len(chunks))
 
     # Vectorize data
     if os.path.exists(vector_db_dir):
         shutil.rmtree(vector_db_dir)
     else:
         os.makedirs(vector_db_dir)
-    vector_store = Chroma.from_documents(splitted_documents,
+    vector_store = Chroma.from_documents(chunks,
                                          embedding=embed_model,
                                          collection_name=collection,
                                          persist_directory=vector_db_dir)
@@ -53,7 +61,7 @@ CACHE_FOLDER = os.getenv('CACHE_FOLDER', './model_dir/')
 VECTOR_DB_DIR = os.getenv('VECTOR_DB_DIR', './data/chroma_db/' + EMBED_MODEL_HF.split('/')[-1])
 VECTOR_DB_CLT = os.getenv('VECTOR_DB_CLT', 'groq_rag')
 MAX_EMBED_TOKEN = int(os.getenv("MAX_EMBED_TOKEN", 8192))
-CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", 100))
+CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", 0))
 
 embed_model = HuggingFaceEmbeddings(model_name=EMBED_MODEL_HF,
                                     model_kwargs={"trust_remote_code": True},
